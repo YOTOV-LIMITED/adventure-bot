b404bd5b6be2a066176d79e4d3afdc251418a1cb
Mathew Trivett <matt.trivett@gmail.com>
2015-03-01 22:38:21 +0000

    Initial commit

--- limbo/plugins/unutils.py
+++ limbo/plugins/unutils.py
@@ -1,14 +1,39 @@
 import re
+import yaml
 
-myInventory = ["12 Blankets", "Book of Mistakes"]
+myInventory = {}
 
 def on_message(msg, server):
-    text = msg.get("text", "")
-    match = re.findall(r"!inventory( )?(.*)?", text)
-    if match:
-        words = match[0][1].split(" ")
-        if words[0] == 'add':
-            myInventory.append(" ".join(words[1:]))
-        if words[0] == 'remove':
-            myInventory.remove(" ".join(words[1:]))
-        return "\nIn your inventory is: \n   " + "\n   ".join(myInventory)
+	text = msg.get("text", "")
+	match = re.findall(r"!inventory( )?(.*)?", text)
+	if match:
+		words = match[0][1].split(" ")
+
+		if words[0] == 'add':
+			if words[1].isdigit():
+				item = (" ".join(words[2:]))
+				quantity = int(words[1])
+
+			else:
+				item = (" ".join(words[1:]))
+				quantity = 1
+			myInventory[item] = quantity
+
+		if words[0] == 'remove':
+			if words[1].isdigit():
+				item = (" ".join(words[2:]))
+				quantity -= int(words[1])
+
+			else:
+				item = (" ".join(words[1:]))
+				quantity -= 1
+			myInventory[item] = quantity
+
+		if words[0] == 'dump':
+			myInventory.clear()
+			return "\nYou have dumped all of your inventory."
+
+		if words[0] == 'length':
+			return "\nIn your inventory there are %d things." % len(myInventory)
+		
+	return "\nIn your inventory is: \n " + yaml.safe_dump(myInventory)
