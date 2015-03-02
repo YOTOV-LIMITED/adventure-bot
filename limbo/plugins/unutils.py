edca5a650783b29aaf0a08f259d6a884482d72d8
Mathew Trivett <matt.trivett@gmail.com>
2015-03-02 09:37:56 +0000

    Got Adventure-Bot adding to the inventory, removing, dumping and querying length

--- limbo/plugins/unutils.py
+++ limbo/plugins/unutils.py
@@ -1,5 +1,6 @@
 import re
 import yaml
+import inflection
 
 myInventory = {}
 
@@ -13,27 +14,33 @@ def on_message(msg, server):
 			if words[1].isdigit():
 				item = (" ".join(words[2:]))
 				quantity = int(words[1])
-
 			else:
 				item = (" ".join(words[1:]))
 				quantity = 1
-			myInventory[item] = quantity
+
+			myInventory[inflection.singularize(item)] = quantity
 
 		if words[0] == 'remove':
 			if words[1].isdigit():
 				item = (" ".join(words[2:]))
-				quantity -= int(words[1])
-
+				quantity = int(words[1])
 			else:
 				item = (" ".join(words[1:]))
-				quantity -= 1
-			myInventory[item] = quantity
+				quantity = 1
+
+			myInventory[inflection.singularize(item)] -= quantity
+			
+			if myInventory[inflection.singularize(item)] == 0:
+				del myInventory[inflection.singularize(item)]
 
 		if words[0] == 'dump':
 			myInventory.clear()
 			return "\nYou have dumped all of your inventory."
 
 		if words[0] == 'length':
-			return "\nIn your inventory there are %d things." % len(myInventory)
+			if len(myInventory) == 0:
+				return "\nYou have nothing in your inventory."
+			else:
+				return "\nIn your inventory there are %d things." % len(myInventory)
 		
 	return "\nIn your inventory is: \n " + yaml.safe_dump(myInventory)
